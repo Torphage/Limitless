1e97c90a9ea8c91fbda788c4597a608b10a516d5
Torphage <filip.torphage1@gmail.com>
2019-05-06 17:55:32 +0200

    added based on classes

--- db/seeder.rb
+++ db/seeder.rb
@@ -1,35 +1,29 @@
 require 'sqlite3'
 require 'pp'
 
+require_relative '../models/model'
+require_relative '../models/user'
+require_relative '../models/document'
+require_relative '../models/document_user'
+require_relative '../models/page'
+
 class Seeder
     def self.seed!
         db = SQLite3::Database.new('db/data.db')
         db.execute('DROP TABLE IF EXISTS users')
-
-        db.execute('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                        firstName VARCHAR(255) NOT NULL,
-                                        lastName VARCHAR(255) NOT NULL,
-                                        email VARCHAR(255) NOT NULL UNIQUE,
-                                        username VARCHAR(255) NOT NULL UNIQUE,
-                                        password VARCHAR(20) NOT NULL,
-                                        profilePic VARCHAR(255))')
-
-        db.execute('DROP TABLE IF EXISTS documents')
-
-        db.execute('CREATE TABLE documents (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                            title VARCHAR(255) NOT NULL,
-                                            textContent VARCHAR(255),
-                                            owner VARCHAR(255) NOT NULL)')
-
-        db.execute('DROP TABLE IF EXISTS users_documents')
-
-        db.execute('CREATE TABLE users_documents (userId INTEGER NOT NULL,
-                                                  documentId INTEGER NOT NULL)')
-                                            
-        pp(db.execute("SELECT * FROM users"))
-        pp(db.execute("SELECT * FROM documents"))
-        pp(db.execute("SELECT * FROM users_documents"))
         
+        db.execute("DROP TABLE IF EXISTS #{User.get_table_name}")
+        db.execute("CREATE TABLE #{User.get_table_name} (#{User.get_columns().zip(User.get_args()).map{ |col,arg| "#{col} #{arg}" }.join(",")})")
+        
+        db.execute("DROP TABLE IF EXISTS #{Document.get_table_name}")
+        db.execute("CREATE TABLE #{Document.get_table_name} (#{Document.get_columns().zip(Document.get_args()).map{ |col,arg| "#{col} #{arg}" }.join(",")})")
+
+        db.execute("DROP TABLE IF EXISTS #{DocumentUser.get_table_name}")
+        db.execute("CREATE TABLE #{DocumentUser.get_table_name} (#{DocumentUser.get_columns().zip(DocumentUser.get_args()).map{ |col,arg| "#{col} #{arg}" }.join(",")})")
+
+        db.execute("DROP TABLE IF EXISTS #{Page.get_table_name}")
+        db.execute("CREATE TABLE #{Page.get_table_name} (#{Page.get_columns().zip(Page.get_args()).map{ |col,arg| "#{col} #{arg}" }.join(",")})")        
+        User.create({firstName: "Guest", lastName: "User", email: "guest@guest.info", username: "GuestUser", password: "guest", profilePic: nil})
     end
     
     def self.print!
@@ -37,7 +31,8 @@ class Seeder
 
         pp(db.execute("SELECT * FROM users"))
         pp(db.execute("SELECT * FROM documents"))
-        pp(db.execute("SELECT * FROM users_documents"))
+        pp(db.execute("SELECT * FROM documents_users"))
+        pp(db.execute("SELECT * FROM pages"))
     end
 end
 
