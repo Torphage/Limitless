f266ad07b9ab7d4ea52bb5eb9c454bb951e717ab
Torphage <filip.torphage1@gmail.com>
2019-05-08 18:43:20 +0200

    Added tests for users + document creation + access

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -1,124 +1,264 @@
 require_relative 'spec_helper'
 
-feature "Registration" do
 
-  scenario "Password Failure" do
-    visit "user/new"
-    expect(page).to have_content "Create account"
-    fill_in 'name', with: 'hej'
-    fill_in 'email', with: 'h@j'
-    fill_in 'pwd', with: '123'
-    click_button 'Create account'
-    expect(page).to have_content "Password is too short"
-  end
 
-  scenario "Name Failure" do
-    visit "user/new"
-    expect(page).to have_content "Create account" 
-    fill_in 'email', with: 'h@j'
-    fill_in 'pwd', with: '1234567'
-    click_button 'Create account'
-    expect(page).to have_content "Name is required"
-  end
+feature("User") do
 
-  scenario "Email Empty Failure" do
-    visit "user/new"
-    expect(page).to have_content "Create account"
-    fill_in 'name', with: 'hej'
-    fill_in 'pwd', with: '1234567'
-    click_button 'Create account'
-    expect(page).to have_content "Email is required"
-  end
+    context("Registration") do
 
-  scenario "Success (from start page)" do
-    visit "/"
-    click_link('Not a member? Sign up here!')
-    expect(page).to have_content "Create account"
-    fill_in 'name', with: 'hej'
-    fill_in 'email', with: 'h@j'
-    fill_in 'pwd', with: '1234567'
-    click_button 'Create account'
-    expect(page).to have_content "Registration successful"
-    click_link('Get back to start by clicking here!')
-  end
+        before(:all) do
+            visit "/signup"
+            within("#signup") do
+                fill_in 'first_name', with: 'First'
+                fill_in 'last_name', with: 'Last'
+                fill_in 'email', with: 'bar@bar.com'
+                fill_in 'username', with: 'Bar'
+                fill_in 'password', with: '123'
+            end
+            click_button('Create account')
+            click_button('Logout')
+        end
+    
+        scenario("Should fail, provided email is not valid") do
+            visit "/"
+            click_link('Sign Up')
+            expect(page).to have_content("Create account")
+            within("#signup") do
+                fill_in 'first_name', with: 'First'
+                fill_in 'last_name', with: 'Last'
+                fill_in 'email', with: 'Foo'
+                fill_in 'username', with: 'Foo'
+                fill_in 'password', with: '123'
+            end
+            click_button('Create account')
+            expect(page).to have_content("Please provide a valid email address.")
+        end
+    
+        scenario("Should fail, username already in use") do
+            visit "/"
+            click_link('Sign Up')
+            expect(page).to have_content("Create account")
+            within("#signup") do
+                fill_in 'first_name', with: 'First'
+                fill_in 'last_name', with: 'Last'
+                fill_in 'email', with: 'foo@bar.com'
+                fill_in 'username', with: 'Bar'
+                fill_in 'password', with: '123'
+            end
+            click_button('Create account')
+            expect(page).to have_content("This username is already in use.")
+        end
+    
+        scenario("Should fail, email is already in use") do
+            visit "/"
+            click_link('Sign Up')
+            expect(page).to have_content("Create account")
+            within("#signup") do
+                fill_in 'first_name', with: 'First'
+                fill_in 'last_name', with: 'Last'
+                fill_in 'email', with: 'bar@bar.com'
+                fill_in 'username', with: 'Foo'
+                fill_in 'password', with: '123'
+            end
+            click_button('Create account')
+            expect(page).to have_content("This email is already in use.")
+        end
+    
+        scenario("Success & Logout") do
+            visit "/"
+            click_link('Sign Up')
+            expect(page).to have_content("Create account")
+            within("#signup") do
+                fill_in 'first_name', with: 'First'
+                fill_in 'last_name', with: 'Last'
+                fill_in 'email', with: 'foo@bar.com'
+                fill_in 'username', with: 'Foo'
+                fill_in 'password', with: '123'
+            end
+            click_button('Create account')
+            expect(page).to have_content("Logout")
+            click_button('Logout')
+        end
+    end
 
-  scenario "Email Failure" do
-    visit "user/new"
-    expect(page).to have_content "Create account"
-    fill_in 'name', with: 'hej'
-    fill_in 'email', with: 'h@j'
-    fill_in 'pwd', with: '1234567'
-    click_button 'Create account'
-    expect(page).to have_content "Email is already used"
-  end
+    context("Login") do
 
+        scenario("Should fail, password is invalid") do
+            visit "/"
+            click_link('Login')
+            expect(page).to have_content("Log In")
+            fill_in 'username', with: 'First'
+            fill_in 'password', with: '123456'
+            click_button('Log In')
+            expect(page).to have_content("Username or password is invalid")
+        end
+    
+        scenario("Should fail, email is invalid") do
+            visit "/"
+            click_link('Login')
+            expect(page).to have_content("Log In")
+            fill_in 'username', with: 'Not Foo'
+            fill_in 'password', with: '123'
+            click_button('Log In')
+            expect(page).to have_content("Username or password is invalid")
+        end
+    
+        scenario("Should pass, Successful") do
+            visit "/"
+            click_link('Login')
+            expect(page).to have_content("Log In")
+            fill_in 'username', with: 'Foo'
+            fill_in 'password', with: '123'
+            click_button('Log In')
+            expect(page).to have_content("Logout")
+            click_button('Logout')
+        end
+    end
+end     
+
+feature("Document") do
+
+    context("Open") do
+        
+        before(:all) do
+            visit "/"
+            click_link('Login')
+            expect(page).to have_content("Log In")
+            within("#login") do
+                fill_in 'username', with: 'Foo'
+                fill_in 'password', with: '123'
+            end
+            click_button('Log In')
+            expect(page).to have_content("Logout")
+
+            visit "/profile/3"
+            expect(page).to have_selector(:link_or_button, "Create Post")
+            within("#create_document") do
+                fill_in 'title', with: 'Doc1'
+            end
+            click_button('Create Post')
+            expect(page).to have_selector(:link_or_button, "Edit")
+            expect(page).to have_content("Doc1")
+            click_button('Logout')
+
+            visit "/"
+            click_link('Login')
+            expect(page).to have_content("Log In")
+            within('#login') do
+                fill_in 'username', with: 'Bar'
+                fill_in 'password', with: '123'
+            end
+            click_button('Log In')
+            expect(page).to have_content("Logout")
+
+            visit "/profile/2"
+            expect(page).to have_selector(:link_or_button, "Create Post")
+            within("#create_document") do
+                fill_in 'title', with: 'Doc2'
+                fill_in 'guests', with: 'Foo'
+            end
+            click_button('Create Post')
+            expect(page).to have_selector(:link_or_button, "Edit")
+            expect(page).to have_content("Doc2")
+            click_button('Logout')
+        end
+
+        scenario("Should pass, allowed: only owner") do
+            visit "/"
+            click_link('Login')
+            expect(page).to have_content("Log In")
+            within("#login") do
+                fill_in 'username', with: 'Foo'
+                fill_in 'password', with: '123'
+            end
+            click_button('Log In')
+            
+            visit "/profile/3"
+            expect(page).to have_content("Doc1")
+            expect(page).to have_selector(:link_or_button, "Edit")
+            click_button('Logout')
+        end
+
+        scenario("Should pass, edit allowed document from another profile") do
+            visit "/"
+            click_link('Login')
+            expect(page).to have_content("Log In")
+            within("#login") do
+                fill_in 'username', with: 'Foo'
+                fill_in 'password', with: '123'
+            end
+            click_button('Log In')
+            
+            visit "/profile/2"
+            expect(page).to have_content("Doc2")
+            expect(page).to have_selector(:link_or_button, "Edit")
+            click_button('Logout')
+        end
+
+        scenario("Should fail, not allowed") do
+            visit "/"
+            click_link('Login')
+            expect(page).to have_content("Log In")
+            fill_in 'username', with: 'Bar'
+            fill_in 'password', with: '123'
+            click_button('Log In')
+            
+            visit "/profile/3"
+            expect(page).to have_content("Doc1")
+            expect(page).to have_no_selector(:link_or_button, "Edit")
+            click_button('Logout')
+        end
+    end
+
+    context("Create") do
+
+        before(:each) do
+            visit "/"
+            click_link('Login')
+            expect(page).to have_content("Log In")
+            within("#login") do
+                fill_in 'username', with: 'Foo'
+                fill_in 'password', with: '123'
+            end
+            click_button('Log In')
+        end
+
+        scenario("Should pass, Only with title") do
+            visit "/profile/3"
+            expect(page).to have_selector(:link_or_button, "Create Post")
+            within("#create_document") do
+                fill_in 'title', with: 'Doc2'
+            end
+            click_button('Create Post')
+            expect(page).to have_selector(:link_or_button, "Edit")
+            expect(page).to have_content("Doc2")
+        end
+
+        scenario("Ahould pass, Add existing user") do
+            visit "/profile/3"
+            expect(page).to have_selector(:link_or_button, "Create Post")
+            within("#create_document") do
+                fill_in 'title', with: 'Doc3'
+                fill_in 'guests', with: 'Bar'
+            end
+            click_button('Create Post')
+            expect(page).to have_selector(:link_or_button, "Edit")
+            expect(page).to have_content("Doc3")
+        end
+
+        scenario("Shoud fail, add non existing user") do
+            visit "/profile/3"
+            expect(page).to have_selector(:link_or_button, "Create Post")
+            within("#create_document") do
+                fill_in 'title', with: 'Doc4'
+                fill_in 'guests', with: 'Bar'
+            end
+            click_button('Create Post')
+            expect(page).to have_selector(:link_or_button, "Edit")
+            expect(page).to have_content("Doc4")
+        end
+    end
 end
-
-feature "Login" do
-
-  scenario "Password failure" do
-    visit "/"
-    expect(page).to have_content "Begin to shop once you're logged in :)"
-    fill_in 'email', with: 'h@j'
-    fill_in 'pwd', with: '123'
-    click_button 'Log in'
-    expect(page).to have_content "Invalid email or password"
-  end
-
-  scenario "Email failure" do
-    visit "/"
-    expect(page).to have_content "Begin to shop once you're logged in :)"
-    fill_in 'email', with: 'h@jj'
-    fill_in 'pwd', with: '1234567'
-    click_button 'Log in'
-    expect(page).to have_content "Invalid email or password"
-  end
-
-  scenario "Successful & Log out" do
-    visit "/"
-    expect(page).to have_content "Begin to shop once you're logged in :)"
-    fill_in 'email', with: 'h@j'
-    fill_in 'pwd', with: '1234567'
-    click_button 'Log in'
-    expect(page).to have_content "You are logged in as:"
-    click_button 'Log out'
-  end
-
-end        # ctrl + *
-
-feature "Cart" do
-
-  scenario "Empty cart" do
-    visit "/"
-    expect(page).to have_content "Begin to shop once you're logged in :)"
-    fill_in 'email', with: 'h@j'
-    fill_in 'pwd', with: '1234567'
-    click_button 'Log in'
-    expect(page).to have_content "You are logged in as:"
-    click_link('Check cart')
-    expect(page).to have_content "No items in cart"
-    click_link('Get back to start by clicking here!')
-    click_button 'Log out'
-  end
-
-  scenario "Add & delete from cart" do
-    visit "/"
-    expect(page).to have_content "Begin to shop once you're logged in :)"
-    fill_in 'email', with: 'h@j'
-    fill_in 'pwd', with: '1234567'
-    click_button 'Log in'
-    expect(page).to have_content "You are logged in as:"
-    click_button 'Add item 1 to cart'
-    click_link('Check cart')
-    expect(page).to have_content "Cart:"
-    click_button 'Delete from cart'
-    expect(page).to have_content "No items in cart"
-    click_link('Get back to start by clicking here!')
-    click_button 'Log out'
-  end
-
-end
-
 # cmd -> app.rb -> bundle exec rspec
 
 
