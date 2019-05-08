3c91e1c8f7d35530f66f73a0d5fa63bde5e403eb
Torphage <filip.torphage1@gmail.com>
2019-05-08 21:26:11 +0200

    Seeder: added dynamic default value builder

--- db/seeder.rb
+++ db/seeder.rb
@@ -8,15 +8,29 @@ require_relative '../models/document_user'
 require_relative '../models/page'
 
 class Seeder
-    def self.seed!        
+    def self.seed!
         User.rebuild_table()
         Document.rebuild_table()
         DocumentUser.rebuild_table()
         Page.rebuild_table()
+        self.standard()
+    end    
 
-        User.create({first_name: "Guest", last_name: "User", email: "guest@guest.info", username: "GuestUser", password: "guest", profile_pic: nil})
+    def self.standard()
+        files = Dir["db/**/*.csv"]
+        files.each do |file|
+            class_name = file.match(/\/([^_]*)\_/).captures[0]
+            values = File.readlines(file)
+            values.shift
+            header = values.shift.strip.split(",")
+            values.each do |row|
+                columns = row.split(",")
+                dict = header.map(&:to_sym).zip(columns).to_h
+                Object.const_get(class_name.capitalize).create(dict)
+            end
+        end
     end
-    
+
     def self.print!
         db = SQLite3::Database.new('db/data.db')
 
