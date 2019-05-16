19d8b996b74de3638c37a4aaf556358628d700ee
Torphage <filip.torphage1@gmail.com>
2019-05-16 10:49:31 +0200

    added @return to Helpers' functions' documentation

--- models/helpers.rb
+++ models/helpers.rb
@@ -3,6 +3,9 @@ module Helpers
     # Store the picture locally and return the file name.
     #
     # @param pic [Object] The file Object of which to store locally here.
+    #
+    # @return [nil] If pic is nil.
+    # @return [String] The file name of the newly stored picture.
     def self.store_pic(pic)
         if pic.nil?
             return nil
